14bf8426ce44ddf54ba53a1c7763327b7c930042
Sirat <sirat@gmail.com>
2023-06-15 05:02:49 +0500

    15 june

---
+++ Sirat/chap4/task3.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(){
+	
+	int f,s,t;
+	
+	printf("Enter a three-digit number: ");
+	scanf("%1d%1d%1d",&f,&s,&t);
+	
+	printf("the reversal is: %d%d%d",t,s,f);
+	
+	return 0;
+}
