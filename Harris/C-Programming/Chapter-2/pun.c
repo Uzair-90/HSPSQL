4826d2f23b2928add5b9f7f591de22093971446d
unknown <harrisabdullah2001@gmail.com>
2023-06-17 17:20:02 +0500

    a program that prints a bad pun

---
+++ Harris/C-Programming/Chapter-2/pun.c
@@ -0,0 +1,12 @@
+/* Name: pun.c
+ * Purpose: Prints a bad pun
+ * Author: Harris
+ */
+
+#include <stdio.h>
+
+int main(void) 
+{
+    printf("To C, or not to C: that is the question. \n");
+    return 0;
+}
