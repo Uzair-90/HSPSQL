b23e98527488d95ff173041e838b20e98d0d6cfd
Harris Abdullah <50470482+harrisabdullahmalik@users.noreply.github.com>
2023-06-17 23:40:06 +0500

    Delete tempCodeRunnerFile.c

--- Harris/C-Programming/Chapter-2/tempCodeRunnerFile.c
+++
@@ -1,9 +0,0 @@
-    // printf("Enter length of the box: ");
-    // scanf("%d", &length);
-    // printf("Enter width of the box: ");
-    // scanf("%d", &width);
-    // printf("Enter heigth of the box: ");
-    // scanf("%d", &height);
-    // volume = length * width * height;
-
-    // printf("Volume of the box is %d\n", volume);
