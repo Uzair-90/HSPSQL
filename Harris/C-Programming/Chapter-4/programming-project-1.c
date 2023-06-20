9d8e805f1f5e0ad8cf3bf03951d3b1398bc41810
unknown <harrisabdullah2001@gmail.com>
2023-06-21 03:25:36 +0500

    uploaded programming project 1 of chapter 4

---
+++ Harris/C-Programming/Chapter-4/programming-project-1.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+    int number, first_digit, second_digit;
+    
+    printf("Enter a two-digit number: ");
+    scanf("%d", &number);
+    first_digit = number / 10;
+    second_digit = number % 10;
+
+    printf("The reversal is: %d%d", second_digit, first_digit);
+
+    return 0;
+}
